389b712e0ee80f4f0fc08d4c3f8d888aec84c25d
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2020-01-22 19:17:01 -0500

    Initial switch to JW player

--- stream-switch.js
+++ stream-switch.js
@@ -1,5 +1,5 @@
-var options = null;
-var player = null;
+// var options = null;
+// var player = null;
 
 var audioUrl = '';
 var videoUrl = '';
@@ -81,28 +81,51 @@ Vue.component('stream-switch', {
 
     methods: {
         selectedAudio: function() {
-            if (this.applicationInsightsInstrumentationKey != '') {
-                options.plugins.appInsights.streamId = this.azureAudioChannelName.toLowerCase();
-            }
+            // if (this.applicationInsightsInstrumentationKey != '') {
+            //     options.plugins.appInsights.streamId = this.azureAudioChannelName.toLowerCase();
+            // }
 
-            player.src([{
-                src: audioUrl,
-                type: 'application/vnd.ms-sstr+xml'
-            }]);
+            
+            // player.src([{
+            //     src: audioUrl,
+            //     type: 'application/vnd.ms-sstr+xml'
+            // }]);
+            
+            var audio = audioUrl + '(format=m3u8-aapl-v3)';
+            
+            jwplayer('stream-switch-jw-player').setup({
+                "file": audio,
+                "aspectratio": "16:9",
+                "autostart": false,
+                "controls": true,
+                "preload": "metadata",
+                "primary": "html5"
+            });
 
             this.audioButtonBackgroundColor = '#CCCCCC';
             this.videoButtonBackgroundColor = 'transparent';
         },
 
         selectedVideo: function() {
-            if (this.applicationInsightsInstrumentationKey != '') {
-                options.plugins.appInsights.streamId = this.azureVideoChannelName.toLowerCase();
-            }
+            // if (this.applicationInsightsInstrumentationKey != '') {
+            //     options.plugins.appInsights.streamId = this.azureVideoChannelName.toLowerCase();
+            // }
 
-            player.src([{
-                src: videoUrl,
-                type: 'application/vnd.ms-sstr+xml'
-            }]);
+            // player.src([{
+            //     src: videoUrl,
+            //     type: 'application/vnd.ms-sstr+xml'
+            // }]);
+
+            var video = videoUrl + '(format=m3u8-aapl-v3)';
+
+            jwplayer('stream-switch-jw-player').setup({
+                "file": video,
+                "aspectratio": "16:9",
+                "autostart": false,
+                "controls": true,
+                "preload": "metadata",
+                "primary": "html5"
+            });
 
             this.audioButtonBackgroundColor = 'transparent';
             this.videoButtonBackgroundColor = '#CCCCCC';
@@ -114,13 +137,13 @@ Vue.component('stream-switch', {
         // region Azure Application Insights
         // Source: https://docs.microsoft.com/en-us/azure/azure-monitor/app/website-monitoring#configure-app-insights-sdk
 
-        if (this.applicationInsightsInstrumentationKey != '') {
-            var sdkInstance="appInsightsSDK";window[sdkInstance]="appInsights";var aiName=window[sdkInstance],aisdk=window[aiName]||function(e){function n(e){t[e]=function(){var n=arguments;t.queue.push(function(){t[e].apply(t,n)})}}var t={config:e};t.initialize=!0;var i=document,a=window;setTimeout(function(){var n=i.createElement("script");n.src=e.url||"https://az416426.vo.msecnd.net/scripts/b/ai.2.min.js",i.getElementsByTagName("script")[0].parentNode.appendChild(n)});try{t.cookie=i.cookie}catch(e){}t.queue=[],t.version=2;for(var r=["Event","PageView","Exception","Trace","DependencyData","Metric","PageViewPerformance"];r.length;)n("track"+r.pop());n("startTrackPage"),n("stopTrackPage");var s="Track"+r[0];if(n("start"+s),n("stop"+s),n("setAuthenticatedUserContext"),n("clearAuthenticatedUserContext"),n("flush"),!(!0===e.disableExceptionTracking||e.extensionConfig&&e.extensionConfig.ApplicationInsightsAnalytics&&!0===e.extensionConfig.ApplicationInsightsAnalytics.disableExceptionTracking)){n("_"+(r="onerror"));var o=a[r];a[r]=function(e,n,i,a,s){var c=o&&o(e,n,i,a,s);return!0!==c&&t["_"+r]({message:e,url:n,lineNumber:i,columnNumber:a,error:s}),c},e.autoExceptionInstrumented=!0}return t}(
-            {
-                instrumentationKey: this.applicationInsightsInstrumentationKey
-            }
-            );window[aiName]=aisdk,aisdk.queue&&0===aisdk.queue.length&&aisdk.trackPageView({});
-        }
+        // if (this.applicationInsightsInstrumentationKey != '') {
+        //     var sdkInstance="appInsightsSDK";window[sdkInstance]="appInsights";var aiName=window[sdkInstance],aisdk=window[aiName]||function(e){function n(e){t[e]=function(){var n=arguments;t.queue.push(function(){t[e].apply(t,n)})}}var t={config:e};t.initialize=!0;var i=document,a=window;setTimeout(function(){var n=i.createElement("script");n.src=e.url||"https://az416426.vo.msecnd.net/scripts/b/ai.2.min.js",i.getElementsByTagName("script")[0].parentNode.appendChild(n)});try{t.cookie=i.cookie}catch(e){}t.queue=[],t.version=2;for(var r=["Event","PageView","Exception","Trace","DependencyData","Metric","PageViewPerformance"];r.length;)n("track"+r.pop());n("startTrackPage"),n("stopTrackPage");var s="Track"+r[0];if(n("start"+s),n("stop"+s),n("setAuthenticatedUserContext"),n("clearAuthenticatedUserContext"),n("flush"),!(!0===e.disableExceptionTracking||e.extensionConfig&&e.extensionConfig.ApplicationInsightsAnalytics&&!0===e.extensionConfig.ApplicationInsightsAnalytics.disableExceptionTracking)){n("_"+(r="onerror"));var o=a[r];a[r]=function(e,n,i,a,s){var c=o&&o(e,n,i,a,s);return!0!==c&&t["_"+r]({message:e,url:n,lineNumber:i,columnNumber:a,error:s}),c},e.autoExceptionInstrumented=!0}return t}(
+        //     {
+        //         instrumentationKey: this.applicationInsightsInstrumentationKey
+        //     }
+        //     );window[aiName]=aisdk,aisdk.queue&&0===aisdk.queue.length&&aisdk.trackPageView({});
+        // }
 
         // endregion
 
@@ -130,62 +153,71 @@ Vue.component('stream-switch', {
         url += this.firebaseDatabaseName;
         url += '/documents/media';
 
-        options = {
-            autoplay: this.autoplay,
-            controls: this.controls,
-            nativeControlsForTouch: this.nativeControlsForTouch,
+        // options = {
+        //     autoplay: this.autoplay,
+        //     controls: this.controls,
+        //     nativeControlsForTouch: this.nativeControlsForTouch,
 
-            logo: {
-                enabled: false
-            },
+        //     logo: {
+        //         enabled: false
+        //     },
 
-            fluid: true,
-            height: this.height,
-            width: this.width
-        };
+        //     fluid: true,
+        //     height: this.height,
+        //     width: this.width
+        // };
 
-        if (this.applicationInsightsInstrumentationKey != '') {
-            options.plugins = {
-                appInsights: {
-                    'streamId': this.azureVideoChannelName.toLowerCase(), // Defaults to the video stream
-                    'metricsToTrack': [
-                        'playbackSummary',
-                        'loaded',
-                        'viewed',
-                        'ended',
-                        'playTime',
-                        'percentsPlayed',
-                        'play',
-                        'pause',
-                        'seek',
-                        'fullscreen',
-                        'error',
-                        'buffering',
-                        'bitrateQuality',
-                        'downloadInfo'
-                    ]
-                }
-            };
-        }
+        // if (this.applicationInsightsInstrumentationKey != '') {
+        //     options.plugins = {
+        //         appInsights: {
+        //             'streamId': this.azureVideoChannelName.toLowerCase(), // Defaults to the video stream
+        //             'metricsToTrack': [
+        //                 'playbackSummary',
+        //                 'loaded',
+        //                 'viewed',
+        //                 'ended',
+        //                 'playTime',
+        //                 'percentsPlayed',
+        //                 'play',
+        //                 'pause',
+        //                 'seek',
+        //                 'fullscreen',
+        //                 'error',
+        //                 'buffering',
+        //                 'bitrateQuality',
+        //                 'downloadInfo'
+        //             ]
+        //         }
+        //     };
+        // }
 
         var error = this.errorMessage;
         var audioChannelName = this.azureAudioChannelName;
         var videoChannelName = this.azureVideoChannelName;
 
-        player = amp('stream-switch-amp', options);
+        // player = amp('stream-switch-amp', options);
 
-        player.addEventListener('error', function () {
-            var message = document.querySelectorAll('#stream-switch-amp div.vjs-modal-dialog-content')[0];
-            message.innerText = error;
-        });
+        // player.addEventListener('error', function () {
+        //     var message = document.querySelectorAll('#stream-switch-amp div.vjs-modal-dialog-content')[0];
+        //     message.innerText = error;
+        // });
 
         axios.get(url)
             .then(function (response) {
                 if (response == null || response.data == null || response.data.documents == null) {
-                    player.src([{
-                        src: '',
-                        type: 'application/vnd.ms-sstr+xml'
-                    }]);
+                    // player.src([{
+                    //     src: '',
+                    //     type: 'application/vnd.ms-sstr+xml'
+                    // }]);
+
+                    jwplayer('stream-switch-jw-player').setup({
+                        "file": '',
+                        "aspectratio": "16:9",
+                        "autostart": false,
+                        "controls": true,
+                        "preload": "metadata",
+                        "primary": "html5"
+                    });
 
                     return;
                 }
@@ -203,18 +235,39 @@ Vue.component('stream-switch', {
                     }
                 });
 
-                player.src([{
-                    src: videoUrl,
-                    type: 'application/vnd.ms-sstr+xml'
-                }]);
+                var video = videoUrl + '(format=m3u8-aapl-v3)';
+
+                jwplayer('stream-switch-jw-player').setup({
+                    "file": video,
+                    "aspectratio": "16:9",
+                    "autostart": false,
+                    "controls": true,
+                    "preload": "metadata",
+                    "primary": "html5"
+                });
+
+                // player.src([{
+                //     src: videoUrl,
+                //     type: 'application/vnd.ms-sstr+xml'
+                // }]);
             })
             .catch(function () {
-                player.src([{
-                    src: '',
-                    type: 'application/vnd.ms-sstr+xml'
-                }]);
+
+                jwplayer('stream-switch-jw-player').setup({
+                    "file": '',
+                    "aspectratio": "16:9",
+                    "autostart": false,
+                    "controls": true,
+                    "preload": "metadata",
+                    "primary": "html5"
+                });
+
+                // player.src([{
+                //     src: '',
+                //     type: 'application/vnd.ms-sstr+xml'
+                // }]);
             });
     },
     
-    template: '<div><video id="stream-switch-amp" class="azuremediaplayer amp-default-skin amp-big-play-centered" tabindex="0" /><div align="center"><ul style="margin: 10px 0 0 0; padding: 0;"><li @click="selectedVideo" :style="videoStyles"><img src="https://cdn.jsdelivr.net/gh/literal-life-church/stream-switch@latest/assets/video.png" width="40"><span style="display: block;">{{ azureVideoChannelName.charAt(0).toUpperCase() + azureVideoChannelName.slice(1).toLowerCase() }}</span></li><li @click="selectedAudio" :style="audioStyles"><img src="https://cdn.jsdelivr.net/gh/literal-life-church/stream-switch@latest/assets/audio.png" width="40"><span style="display: block;">{{ azureAudioChannelName.charAt(0).toUpperCase() + azureAudioChannelName.slice(1).toLowerCase() }}</span></li></ul></div></div>'
+    template: '<div><video id="stream-switch-jw-player" /><div align="center"><ul style="margin: 10px 0 0 0; padding: 0;"><li @click="selectedVideo" :style="videoStyles"><img src="https://cdn.jsdelivr.net/gh/literal-life-church/stream-switch@latest/assets/video.png" width="40"><span style="display: block;">{{ azureVideoChannelName.charAt(0).toUpperCase() + azureVideoChannelName.slice(1).toLowerCase() }}</span></li><li @click="selectedAudio" :style="audioStyles"><img src="https://cdn.jsdelivr.net/gh/literal-life-church/stream-switch@latest/assets/audio.png" width="40"><span style="display: block;">{{ azureAudioChannelName.charAt(0).toUpperCase() + azureAudioChannelName.slice(1).toLowerCase() }}</span></li></ul></div></div>'
 });
