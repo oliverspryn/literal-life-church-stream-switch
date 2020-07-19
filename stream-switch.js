faec445091af7c6fd56ea70f4f5ec95816877b7c
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2020-07-19 09:48:12 -0400

    Add manifest polling

--- stream-switch.js
+++ stream-switch.js
@@ -45,6 +45,10 @@ Vue.component("stream-switch", {
         placeholderImage: {
             required: true,
             type: String
+        },
+        pollingInterval: {
+            default: 0,
+            type: Number
         }
     },
 
@@ -56,6 +60,7 @@ Vue.component("stream-switch", {
             showSpinner: false,
             showSwitcherControls: false,
             showVideoPlayer: false,
+            timer: null,
             videoButtonBackgroundColor: "#CCCCCC",
             videoUrls: { dash: "", hls: "" }
         };
@@ -149,6 +154,82 @@ Vue.component("stream-switch", {
             });
         },
 
+        loadManifest: function() {
+            var url = this.generateIngestUrl();
+            var vm = this;
+
+            this.dynamicMessage = this.loadingText;
+            this.showSpinner = true;
+            this.showSwitcherControls = false;
+            this.showVideoPlayer = false;
+
+            axios.get(url)
+                .then(function (response) {
+                    if (response === null || response.data === null || response.data.events === null) {
+                        return;
+                    }
+
+                    var hasAudioUrl = false;
+                    var hasVideoUrl = false;
+
+                    response.data.events.forEach(function (liveEvent) {
+                        if (!liveEvent.isLive) {
+                            return;
+                        }
+
+                        liveEvent.locators.forEach(function (locator) {
+                            if (liveEvent.name.toLowerCase() === vm.azureAudioChannelName.toLowerCase()) {
+                                hasAudioUrl = true;
+
+                                if (locator.type.toLowerCase() === "dash") {
+                                    vm.audioUrls.dash = locator.url;
+                                }
+
+                                if (locator.type.toLowerCase() === "hls") {
+                                    vm.audioUrls.hls = locator.url;
+                                }
+                            }
+
+                            if (liveEvent.name.toLowerCase() === vm.azureVideoChannelName.toLowerCase()) {
+                                hasVideoUrl = true;
+
+                                if (locator.type.toLowerCase() === "dash") {
+                                    vm.videoUrls.dash = locator.url;
+                                }
+
+                                if (locator.type.toLowerCase() === "hls") {
+                                    vm.videoUrls.hls = locator.url;
+                                }
+                            }
+                        });
+                    });
+
+                    vm.showSpinner = false;
+
+                    if (!hasAudioUrl && !hasVideoUrl) {
+                        vm.dynamicMessage = vm.offlineText;
+                        return;
+                    }
+
+                    vm.showSwitcherControls = response.data.isAllLive;
+                    vm.showVideoPlayer = response.data.isAnyLive;
+
+                    if (hasVideoUrl) {
+                        vm.selectedVideo();
+                    } else {
+                        vm.selectedAudio();
+                    }
+
+                    if (vm.timer != null) {
+                        clearInterval(vm.timer);
+                    }
+                })
+                .catch(function () {
+                    vm.dynamicMessage = this.offlineText;
+                    vm.showSpinner = false;
+                });
+        },
+
         selectedAudio: function () {
             var playlist = this.generatePlaylist(
                 this.azureAudioChannelName,
@@ -175,75 +256,12 @@ Vue.component("stream-switch", {
     },
 
     mounted: function () {
-        var url = this.generateIngestUrl();
-        var vm = this;
+        this.loadManifest();
 
-        this.dynamicMessage = this.loadingText;
-        this.showSpinner = true;
-        this.showSwitcherControls = false;
-        this.showVideoPlayer = false;
-
-        axios.get(url)
-            .then(function (response) {
-                if (response === null || response.data === null || response.data.events === null) {
-                    return;
-                }
-
-                var hasAudioUrl = false;
-                var hasVideoUrl = false;
-
-                response.data.events.forEach(function (liveEvent) {
-                    if (!liveEvent.isLive) {
-                        return;
-                    }
-
-                    liveEvent.locators.forEach(function (locator) {
-                        if (liveEvent.name.toLowerCase() === vm.azureAudioChannelName.toLowerCase()) {
-                            hasAudioUrl = true;
-
-                            if (locator.type.toLowerCase() === "dash") {
-                                vm.audioUrls.dash = locator.url;
-                            }
-
-                            if (locator.type.toLowerCase() === "hls") {
-                                vm.audioUrls.hls = locator.url;
-                            }
-                        }
-
-                        if (liveEvent.name.toLowerCase() === vm.azureVideoChannelName.toLowerCase()) {
-                            hasVideoUrl = true;
-
-                            if (locator.type.toLowerCase() === "dash") {
-                                vm.videoUrls.dash = locator.url;
-                            }
-
-                            if (locator.type.toLowerCase() === "hls") {
-                                vm.videoUrls.hls = locator.url;
-                            }
-                        }
-                    });
-                });
-
-                vm.showSpinner = false;
-
-                if (!hasAudioUrl && !hasVideoUrl) {
-                    vm.dynamicMessage = vm.offlineText;
-                    return;
-                }
-
-                vm.showSwitcherControls = response.data.isAllLive;
-                vm.showVideoPlayer = response.data.isAnyLive;
-
-                if (hasVideoUrl) {
-                    vm.selectedVideo();
-                } else {
-                    vm.selectedAudio();
-                }
-            })
-            .catch(function () {
-                vm.dynamicMessage = this.offlineText;
-                vm.showSpinner = false;
-            });
+        if (this.pollingInterval >= 15) {
+            var time = this.pollingInterval * 1000;
+            this.timer = setInterval(this.loadManifest, time);
+        }
     },
 
     template: "<div align=\"center\"><div v-if=\"!showVideoPlayer\"><div style=\"float: left; position: relative;\"><img :src=\"placeholderImage\" /><div style=\"bottom: 0px; left: 0px; position: absolute; right: 0px; top: 0px;\"><div style=\"height: 50%; max-height: 50%; min-height: 50%; width: 100%;\"></div><div style=\"height: 50%; max-height: 50%; min-height: 50%; width: 100%;\"><div style=\"align-items: center; display: flex; height: 100%; justify-content: center; width: 100%;\"><div><i class=\"fa fa-spinner fa-pulse fa-3x fa-fw\" v-if=\"showSpinner\"></i><span class=\"sr-only\" v-if=\"showSpinner\">>Loading...</span><p style=\"font-weight: bold; font-size: 24px;\">{{ dynamicMessage }}</p></div></div></div></div></div></div><div :style=\"playerStyle\"><video id=\"stream-switch-jw-player\" /></div><div align=\"center\" v-if=\"showSwitcherControls\"><ul style=\"margin: 10px 0 0 0; padding: 0;\"><li @click=\"selectedVideo\" :style=\"videoStyles\"><i aria-hidden=\"true\" class=\"fa fa-film\" style=\"font-size: 2em; padding-bottom: 8px;\"></i><span style=\"display: block;\">{{ azureVideoChannelLabel }}</span></li><li @click=\"selectedAudio\" :style=\"audioStyles\"><i aria-hidden=\"true\" class=\"fa fa-volume-up\" style=\"font-size: 2em; padding-bottom: 8px;\"></i><span style=\"display: block;\">{{ azureAudioChannelLabel }}</span></li></ul></div></div>"
